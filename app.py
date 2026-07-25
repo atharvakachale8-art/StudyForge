74d32bc12f8dab56bef8631906eec4a8d8c5bf23
AtharvaKachale <atharva.kachale_it25@pccoer.in>
2026-07-25 23:29:17 +0530

    Created Flask project structure and Homepage

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+print("Starting Program...")
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+    return "<h1>Welcome to StudyForge</h1>"
+
+print("Before app.run()")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    print("This should never print")
